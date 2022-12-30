a9d37c247ff637139d0f4315ad2c4e8e742e024e
heebum1234 <huibeomjeong99@gmail.com>
2022-12-30 16:45:34 +0900

    demo1

---
+++ demo.php
@@ -0,0 +1,126 @@
+<!DOCTYPE html>
+
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no, maximum-scale=1.0, minimum-scale=1.0">
+    <meta name="format-detection" content="telephone=no">
+    <link rel="stylesheet" href="..\demo.css">
+
+  </head>
+
+  <body>
+    <div id="header" style="text-align : center;">
+      <div class="logo">
+        <a href="..\frontpage.html">
+          <img src="..\logos\little logo.png"
+            style="max-width: 25%; margin-top: 30px;"
+            alt="폰바라기">
+        </a>
+      </div>
+<!--
+      <div class="header2" style="text-align : center;">
+            <img class="logo"
+              src="..\logos\skt_img.png"
+              style="max-width: 10%; height: auto;"
+              alt="SKT LOGO">
+      </div>
+      -->
+    </div>
+
+
+    <div id="menuHeadA" style="position: static;">
+    	<div class="menuHeadA_box">
+        <!--
+    		<img src="..." class="typeA_btn_back">
+    		<img src="..." class="typeA_btn_top">
+      ...에 주소-->
+    		<ul class="typeA_menu">
+    			<li style="margin-left: 20px; margin-right: 20px;"><a href="..\salespages\sk_sales.html">SKT</a></li>
+    			<li style="margin-left: 20px; margin-right: 20px;"><a href="..\salespages\lg_sales.html">LGU+</a></li>
+    			<li style="margin-left: 20px; margin-right: 20px;"><a href="..\salespages\kt_sales.html">KT</a></li>
+    			<li style="margin-left: 20px; margin-right: 20px;"><a href="..\salespages\sk_sales.html">갤럭시</a></li>
+    			<li style="margin-left: 20px; margin-right: 20px;"><a href="..\salespages\sk_sales.html">아이폰</a></li>
+    			<li style="margin-left: 20px; margin-right: 20px;"><a href="..\salespages\sk_sales.html">공짜폰</a></li>
+
+        </ul>
+
+      <div class="clear"></div>
+    	</div>
+    </div>
+
+
+<!--
+      <div class="pre-approval" style="text-align : center; margin-top: 10px; margin-bottom: 10px;">
+        <img class="img"
+          src="..\img\pre_approval.png"
+          alt="사전승낙서"
+          style="max-width: 80%; height: auto;">
+      </div>
+      <div class="gift" style="text-align : center; margin-top: 10px; margin-bottom: 10px;">
+        <img class="img"
+          src="..\img\gift.png"
+          alt="사은품"
+          style="max-width: 80%; height: auto;">
+      </div>
+      <div class="kakao" style="text-align : center; margin-top: 10px; margin-bottom: 10px;">
+        <img class="img"
+          src="..\img\kakao.png"
+          alt="카카오톡 상담"
+          style="max-width: 80%; height: auto;">
+      </div>
+      <div class="clear"></div>
+    -->
+
+
+
+
+
+<section id="content_box">
+  <div class="box">
+    <ul class="items">
+        <li><img src="../img/iphone/13.jpg"></li>
+        <li class="pname">아이폰 13 128GB</li>
+        <li class="pprice">출고가 000,000원</li>
+        <li class="dcpprice">구매가 000,000원</li>
+      </ul>
+      <ul class="items">
+        <li><img src="../img/iphone/13.jpg"></li>
+        <li class="pname">아이폰 13 128GB</li>
+        <li class="pprice">출고가 000,000원</li>
+        <li class="dcpprice">구매가 000,000원</li>
+      </ul>
+      <ul class="items">
+        <li><img src="../img/iphone/13.jpg"></li>
+        <li class="pname">아이폰 13 128GB</li>
+        <li class="pprice">출고가 000,000원</li>
+        <li class="dcpprice">구매가 000,000원</li>
+      </ul>
+      <ul class="items">
+        <li><img src="../img/iphone/13.jpg"></li>
+        <li class="pname">아이폰 13 128GB</li>
+        <li class="pprice">출고가 000,000원</li>
+        <li class="dcpprice">구매가 000,000원</li>
+      </ul>
+      <ul class="items">
+        <li><img src="../img/iphone/13.jpg"></li>
+        <li class="pname">아이폰 13 128GB</li>
+        <li class="pprice">출고가 000,000원</li>
+        <li class="dcpprice">구매가 000,000원</li>
+      </ul>
+      <ul class="items">
+        <li><img src="../img/iphone/13.jpg"></li>
+        <li class="pname">아이폰 13 128GB</li>
+        <li class="pprice">출고가 000,000원</li>
+        <li class="dcpprice">구매가 000,000원</li>
+      </ul>
+</div>
+
+<div class="clear"></div>
+</section>
+
+
+  </body>
+
+</html>
